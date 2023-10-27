6125ae9519d51c8fcae3e0224b516c643973a325
Cedrouseroll Omondi <omondicedo@gmail.com>
2023-10-27 17:45:16 +0300

    add shortcuts

--- frontend/uno.config.ts
+++ frontend/uno.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig, presetIcons, presetTypography, presetUno } from 'unocss'
 
 export default defineConfig({
+  shortcuts: [
+    {
+      'footer-text': 'font-normal text-15px',
+      'footer-link': 'hover:underline'
+    }
+  ],
   presets: [
     presetUno(),
     presetIcons(),
