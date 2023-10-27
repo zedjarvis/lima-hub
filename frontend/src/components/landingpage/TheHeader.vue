30e88e0b09224ea867ccc40fcab437db099606f0
Cedrouseroll Omondi <omondicedo@gmail.com>
2023-10-27 18:26:53 +0300

    feat: added responsive mobile nav

--- frontend/src/components/landingpage/TheHeader.vue
+++ frontend/src/components/landingpage/TheHeader.vue
@@ -1,25 +1,41 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+const drawer = ref(false)
+</script>
 
 <template lang="pug">
-VAppBar(flat scroll-behavior="elevate hide" class="px-md-16")
+VAppBar(flat scroll-behavior="elevate hide" class="px-md-16 border-b")
   VAppBarTitle
     VImg(src="/images/logo-green.png" width="130")
 
   VSpacer
 
-  VBtn(variant="plain" :ripple="false") Home
-  VBtn(variant="plain" :ripple="false") About
-  VBtn(variant="plain" :ripple="false") Projects
-  VBtn(variant="plain" :ripple="false") Resources
-  VBtn(variant="plain" :ripple="false")
+  VBtn(variant="plain" :ripple="false" class="d-none d-md-flex") Home
+  VBtn(variant="plain" :ripple="false" class="d-none d-md-flex") About
+  VBtn(variant="plain" :ripple="false" class="d-none d-md-flex") Projects
+  VBtn(variant="plain" :ripple="false" class="d-none d-md-flex") Resources
+  VBtn(variant="plain" :ripple="false" class="d-none d-md-flex")
     | Shop
     VBadge(content="2" color="brand-dark")
       VIcon(icon="mdi-shopping-outline")
 
   VSpacer
 
-  VBtn(rounded="0" class="text-capitalize mx-3") Log In
-  VBtn(rounded="0" class="text-capitalize" variant="flat" color="brand-dark")
+  VBtn(rounded="0" class="text-capitalize mx-3 d-none d-md-flex") Log In
+  VBtn(rounded="0" class="text-capitalize d-none d-md-flex" variant="flat" color="brand-dark")
     | Invest Now
-  //- VIcon(icon="mdi-shopping-outline" class="position-absolute right-10")
+  VAppBarNavIcon(@click="drawer = !drawer" class="d-flex d-md-none")
+
+VNavigationDrawer(v-model="drawer" temporary location="top" class="h-max!")
+  VList(class="px-5 pb-10 space-y-4!")
+    VListItem(title="Home")
+    VListItem(title="About")
+    VListItem(title="Projects")
+    VListItem(title="Resources")
+    VListItem(title="Shop")
+      template(v-slot:append)
+        VBadge(content="2" color="brand-light")
+          VIcon(icon="mdi-shopping-outline")
+    VBtn(variant="outlined" color="brand-dark" rounded="0" block) Log In
+    VBtn(variant="flat" color="brand-dark" rounded="0" block) Invest Now
 </template>
